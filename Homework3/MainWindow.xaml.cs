669c20d6c50d217c0fa7782fecb67ba5251d901b
s_moyar <sergio.moya.roman@gmail.com>
2017-03-12 19:23:07 -0700

    Homework3, March 12th 2017

---
+++ Homework3/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Homework3.Models;
+using System.ComponentModel;
+
+namespace Homework3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<Models.User> users = new List<Models.User>();
+
+            users.Add(new Models.User { Name = "Dave", Password = "1DavePwd" });
+            users.Add(new Models.User { Name = "Steve", Password = "2StevePwd" });
+            users.Add(new Models.User { Name = "Lisa", Password = "3LisaPwd" });
+
+            uxList.ItemsSource = users;
+            // CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            //   view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+            //  view.SortDescriptions.Add(new SortDescription("Password", ListSortDirection.Ascending));
+        }
+
+        private void GridViewNameColumn_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription("Name", ListSortDirection.Ascending));
+
+        }
+
+        private void GridViewPasswordColumn_Click(object sender, RoutedEventArgs e)
+        {
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(uxList.ItemsSource);
+
+            view.SortDescriptions.Add(new SortDescription("Password", ListSortDirection.Ascending));
+        }
+    }
+}
