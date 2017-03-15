3938897c99aa8cc1555ee8ead36fbda4f925d41e
s_moyar <sergio.moya.roman@gmail.com>
2017-03-14 17:02:33 -0700

    Stopwatch exercise

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myStopwatch = new Stopwatch();
+
+            /* Console.WriteLine("Please enter to start stopwatch");
+             Console.ReadLine();
+             myStopwatch.Start();
+
+             Console.WriteLine("Please enter to start stopwatch");
+             Console.ReadLine();
+             myStopwatch.Start();
+             */
+            Console.WriteLine("Please enter to start stopwatch");
+            Console.ReadLine();
+            myStopwatch.Start();
+
+            Console.WriteLine("Please enter to stop stopwatch");
+            Console.ReadLine();
+            myStopwatch.Stop();
+
+            Console.WriteLine("Please enter to stop stopwatch");
+            Console.ReadLine();
+            myStopwatch.Stop();
+
+            Console.WriteLine($"Duration is : {myStopwatch.Duration}");
+            }
+        }
+    }
---
+++ Stopwatch/Stopwatch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    class Stopwatch
+    {
+        private TimeSpan _startTime;
+        private TimeSpan _stopTime;
+        private TimeSpan _duration;
+        private bool _on = false;
+        public void Start()
+        {
+            if (_on) throw (new InvalidOperationException());
+            else
+            {
+                this._startTime = DateTime.Now.TimeOfDay;
+                _on = true;
+            }             
+        }
+        public void Stop()
+        {
+            if (!_on) throw new InvalidOperationException();
+            else
+            {
+                this._stopTime = DateTime.Now.TimeOfDay;
+                _on = false;
+            }
+        }
+        public TimeSpan Duration
+        {
+            get
+            {   _duration = _stopTime - _startTime;
+                return _duration;
+            }
+            private set{_duration = value;}
+        }
+    }
+}
